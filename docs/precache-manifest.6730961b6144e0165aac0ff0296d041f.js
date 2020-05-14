313e89bbb76fba2f9ff5299f55e8a8698ce0f26a
Joy Liu <liukang.joy@gmail.com>
2020-05-14 09:59:36 -0500

    builds storybook in /docs folder

---
+++ docs/precache-manifest.6730961b6144e0165aac0ff0296d041f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f0bbc55bf979399a6f65cb326a9e250",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.6d227afe2e2abe055cbf.bundle.js"
+  },
+  {
+    "url": "runtime~main.6d227afe2e2abe055cbf.bundle.js"
+  },
+  {
+    "revision": "439f83b8279f0f35ebe9",
+    "url": "static/css/main.0ff23084.chunk.css"
+  },
+  {
+    "url": "vendors~main.6d227afe2e2abe055cbf.bundle.js"
+  },
+  {
+    "url": "vendors~main.6d227afe2e2abe055cbf.bundle.js.LICENSE.txt"
+  }
+]);
